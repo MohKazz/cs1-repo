4e189e0932fb8aa55f9ad85e4efc019c7b3b83e4
Mohammed-khazzan <540149@student.fontys.nl>
2025-09-23 21:19:35 +0200

    pushing the terraform state to a remote s3 to automatically run the deploy form GitHub acions

--- backend.tf
+++ backend.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "cs1nca-tfstate-131464424832-eu-central-1"
+    key            = "cs1/dev/terraform.tfstate"   # path inside the bucket
+    region         = "eu-central-1"
+    dynamodb_table = "cs1nca-tflock"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
